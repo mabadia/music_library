ab492a0571c3f34ee0ea67b96b669244d13488c5
mabadia <angel.beyou95@gmail.com>
2023-08-27 18:50:11 -0700

    withContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+
 
 function App() {
   let [search, setSearch] = useState('')
@@ -31,16 +33,16 @@ function App() {
   }
 
   return (
-    <div>
+    <div className="App">
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data} >
+        <Gallery />
+      </DataContext.Provider>
     </div>
   )
 }
 
 export default App
-
-
 
 
