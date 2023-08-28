05a46db5a9fa22aa07b1b7c83e76c601385fab4c
mabadia <angel.beyou95@gmail.com>
2023-08-27 17:41:54 -0700

    part 2

--- src/App.js
+++ src/App.js
@@ -6,28 +6,41 @@ function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
   useEffect(() => {
-    const fetchData = async () => {
-      document.title = `${search}Music`
-      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead'
-      )
-      const resData = await response.json()
-      if (resData.results.length) {
-        setData(resData.results)
-      } else{
-        setMessage('Not Found')
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search}Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
       }
+      fetchData()
     }
-    fetchData()
   }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
 
   return (
     <div>
-      <SearchBar />
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <Gallery data={data} />
     </div>
   )
 }
 
 export default App
+
+
+
+
