681d12413623962fbad24eb5039ecd70a4a76aae
mabadia <angel.beyou95@gmail.com>
2023-08-28 21:38:31 -0700

    complete with_router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState, useRef } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
-import { SearchContext } from './context/SearchContext'
+// import { DataContext } from './context/DataContext'
+// import { SearchContext } from './context/SearchContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 
 function App() {
@@ -28,19 +31,24 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
+    <div>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <SearchBar handleSearch={handleSearch} />
+              <Gallery data={data} />
+            </>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   )
+
 }
 
 export default App
+
