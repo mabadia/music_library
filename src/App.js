8cef4429f9d64501610c50729a467af9dbaae343
mabadia <angel.beyou95@gmail.com>
2023-09-17 20:13:56 -0700

    day 6 improve a music library interface

--- src/App.js
+++ src/App.js
@@ -8,26 +8,32 @@ import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
 
 
+
 function App() {
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
   let searchInput = useRef('')
   const API_URL = 'https://itunes.apple.com/search?term='
 
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${term}Music`
+        const response = await fetch(API_URL + term)
+        const resData = await response.json()
+        if (resData.results.length) {
+          return setData(resData.results)
+        } else {
+          return setMessage('Not Found')
+        }
+      }
+      fetchData()
+    }
+  }, [search])
 
   const handleSearch = (e, term) => {
-    const fetchData = async () => {
-      document.title = `${term}Music`
-      const response = await fetch(API_URL + term)
-      const resData = await response.json()
-      if (resData.results.length) {
-        return setData(resData.results)
-      } else {
-        return setMessage('Not Found')
-      }
-    }
-    fetchData()
     e.preventDefault()
+    setSearch(term)
   }
 
   return (
